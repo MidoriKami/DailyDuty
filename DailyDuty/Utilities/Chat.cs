f9f11c6088de647c7cb24578402ea728c4cd78f1
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-02-27 16:51:55 -0800

    Add debug helper

--- DailyDuty/Utilities/Chat.cs
+++ DailyDuty/Utilities/Chat.cs
@@ -1,5 +1,8 @@
-﻿using Dalamud.Game.Text.SeStringHandling;
+﻿using System;
+using System.Runtime.InteropServices;
+using Dalamud.Game.Text.SeStringHandling;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace DailyDuty.Utilities
 {
@@ -49,6 +52,29 @@ namespace DailyDuty.Utilities
         public static void Debug(string data)
         {
             Print("Debug", data);
+        }
+
+        public static unsafe void PrintAtkEvent(AtkEvent* pointer)
+        {
+            if(pointer == null) return;
+            
+            var node = pointer->Node;
+            var target = pointer->Target;
+            var listener = pointer->Listener;
+            var param = pointer->Param;
+            var nextEvent = pointer->NextEvent;
+            var type = pointer->Type;
+            var unk = pointer->Unk29;
+            var flags = pointer->Flags;
+            
+            Debug($"Node:{(IntPtr)node:X8}");
+            Debug($"Target:{(IntPtr)target:X8}");
+            Debug($"Listener:{(IntPtr)listener:X8}");
+            Debug($"Param:{param}");
+            Debug($"NextEvent:{(IntPtr)nextEvent:X8}");
+            Debug($"Type:{type}");
+            Debug($"Unk:{unk}");
+            Debug($"Flags:{flags}");
         }
     }
 }
