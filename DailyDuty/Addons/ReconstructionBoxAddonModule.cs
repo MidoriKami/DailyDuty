44440617079b810c538ede53aea84e45f305116b
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-03-09 22:31:13 -0800

    DomanEnclave Logic Improvements

--- DailyDuty/Addons/ReconstructionBoxAddonModule.cs
+++ DailyDuty/Addons/ReconstructionBoxAddonModule.cs
@@ -27,10 +27,12 @@ namespace DailyDuty.Addons
         public AddonName AddonName => AddonName.ReconstructionBox;
         private DomanEnclaveSettings Settings => Service.Configuration.Current().DomanEnclave;
 
-        private delegate byte EventHandle(AtkUnitBase* atkUnitBase, AtkEventType eventType, uint eventParam, AtkEvent* atkEvent, void* a5);
+        private delegate byte EventHandle(AtkUnitBase* atkUnitBase, AtkEventType eventType, uint eventParam, AtkEvent* atkEvent, MouseClickEventData* a5);
+        private delegate void* OnSetup(AtkUnitBase* atkUnitBase, int a2, void* a3);
         private delegate void* Finalize(AtkUnitBase* atkUnitBase);
 
         private Hook<EventHandle>? eventHandleHook = null;
+        private Hook<OnSetup>? onSetupHook = null;
         private Hook<Finalize>? finalizeHook = null;
 
         private bool depositButtonPressed = false;
@@ -46,6 +48,7 @@ namespace DailyDuty.Addons
         {
             Service.Framework.Update -= FrameworkOnUpdate;
 
+            onSetupHook?.Dispose();
             eventHandleHook?.Dispose();
             finalizeHook?.Dispose();
         }
@@ -58,84 +61,82 @@ namespace DailyDuty.Addons
             var depositButton = GetDepositButton();
 
             if (addonPointer == null || depositButton == null) return;
-            
+
+            var setupPointer = addonPointer->AtkEventListener.vfunc[45];
+            var eventHandlerPointer = depositButton->AtkEventListener.vfunc[2];
             var finalizePointer = addonPointer->AtkEventListener.vfunc[38];
-            var eventHandlePointer = depositButton->AtkEventListener.vfunc[2];
 
-            eventHandleHook = new Hook<EventHandle>(new IntPtr(eventHandlePointer), OnButtonEvent);
-            finalizeHook = new Hook<Finalize>(new IntPtr(finalizePointer), OnFinalize);
+            onSetupHook = new Hook<OnSetup>(new IntPtr(setupPointer), OnSetupHandler);
+            eventHandleHook = new Hook<EventHandle>(new IntPtr(eventHandlerPointer), EventHandler);
+            finalizeHook = new Hook<Finalize>(new IntPtr(finalizePointer), FinalizeHandler);
 
+            onSetupHook.Enable();
             eventHandleHook.Enable();
             finalizeHook.Enable();
 
             Service.Framework.Update -= FrameworkOnUpdate;
         }
 
-        private void* OnFinalize(AtkUnitBase* atkUnitBase)
+        private void* OnSetupHandler(AtkUnitBase* atkUnitBase, int a2, void* a3)
         {
-            if (Settings.Enabled)
-            {
-                if (atkUnitBase == addonAddress)
-                {
-                    Chat.Debug("ReconstructionBox::Finalize");
+            depositButtonPressed = false;
+            depositAmount = 0;
+            addonAddress = atkUnitBase;
 
-                    var yesNoState = AddonManager.YesNoAddonHelper.GetLastState();
-                    var yesPopupSelected = yesNoState == SelectYesNoAddonHelper.ButtonState.Yes;
+            AddonManager.YesNoAddonHelper.ResetState();
 
-                    if (depositButtonPressed && yesPopupSelected)
-                    {
-                        if (Settings.ShowTrackedDonationAmount)
-                        {
-                            Chat.Print("DonationAmount", $"{depositAmount:n0} gil donated to Doman Enclave");
-                        }
-
-                        depositButtonPressed = false;
-                        Settings.CurrentEarnings += depositAmount;
-                        Service.Configuration.Save();
-                    }
-                }
-            }
-            
-            return finalizeHook!.Original(atkUnitBase);
+            return onSetupHook!.Original(atkUnitBase, a2, a3);
         }
-
-        private byte OnButtonEvent(AtkUnitBase* atkUnitBase, AtkEventType eventType, uint eventParam, AtkEvent* atkEvent, void* a5)
+        
+        private byte EventHandler(AtkUnitBase* atkUnitBase, AtkEventType eventType, uint eventParam, AtkEvent* atkEvent, MouseClickEventData* a5)
         {
-            if (Settings.Enabled)
+            // If this module is enabled
+            if (Settings.Enabled && IsReconstructionBoxOpen())
             {
-                if (atkUnitBase == GetCloseButton() || atkUnitBase == GetDepositButton())
+                switch (eventType)
                 {
-                    if (eventType == AtkEventType.MouseDown)
-                    {
+                    case AtkEventType.MouseDown when a5->RightClick == false && atkUnitBase == GetDepositButton():
+
                         var button = (AtkComponentButton*) atkUnitBase;
 
                         if (button->IsEnabled)
                         {
-                            var eventData = (MouseClickEventData*) a5;
+                            depositButtonPressed = true;
+                            depositAmount = GetGrandTotal();
 
-                            if (eventData->RightClick == false)
-                            {
-                                if (atkUnitBase == GetCloseButton())
-                                {
-                                    depositButtonPressed = false;
-                                    depositAmount = 0;
-                                    AddonManager.YesNoAddonHelper.ResetState();
-                                }
-
-                                if (atkUnitBase == GetDepositButton())
-                                {
-                                    depositButtonPressed = true;
-                                    depositAmount = GetGrandTotal();
-                                    addonAddress = GetAddonPointer();
-                                    AddonManager.YesNoAddonHelper.ResetState();
-                                }
-                            }
+                            AddonManager.YesNoAddonHelper.ResetState();
                         }
-                    }
+                        break;
+
+                    default:
+                        break;
                 }
             }
 
             return eventHandleHook!.Original(atkUnitBase, eventType, eventParam, atkEvent, a5);
+        }
+
+        private void* FinalizeHandler(AtkUnitBase* atkUnitBase)
+        {
+            if (Settings.Enabled && atkUnitBase == addonAddress)
+            {
+                var yesNoState = AddonManager.YesNoAddonHelper.GetLastState();
+                var yesPopupSelected = yesNoState == SelectYesNoAddonHelper.ButtonState.Yes;
+
+                if (depositButtonPressed && yesPopupSelected)
+                {
+                    if (Settings.ShowTrackedDonationAmount)
+                    {
+                        Chat.Print("DonationAmount", $"{depositAmount:n0} gil donated to Doman Enclave");
+                    }
+
+                    depositButtonPressed = false;
+                    Settings.CurrentEarnings += depositAmount;
+                    Service.Configuration.Save();
+                }
+            }
+            
+            return finalizeHook!.Original(atkUnitBase);
         }
 
         //
