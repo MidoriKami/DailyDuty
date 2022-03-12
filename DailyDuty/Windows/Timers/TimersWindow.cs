05c427ef88662a7d05aab7531d165549a128556e
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-03-11 22:39:36 -0800

    timers window cleanup

--- DailyDuty/Windows/Timers/TimersWindow.cs
+++ DailyDuty/Windows/Timers/TimersWindow.cs
@@ -5,6 +5,7 @@ using DailyDuty.Data.Enums;
 using DailyDuty.Data.SettingsObjects.Windows;
 using DailyDuty.Interfaces;
 using DailyDuty.Timers;
+using DailyDuty.Utilities;
 using Dalamud.Game;
 using Dalamud.Game.ClientState.Conditions;
 using Dalamud.Interface.Windowing;
@@ -12,70 +13,57 @@ using ImGuiNET;
 
 namespace DailyDuty.Windows.Timers
 {
-    internal class TimersWindow : Window, IDisposable, IWindow
+    internal class TimersWindow : Window, IWindow
     {
         private readonly CountdownTimers countdownTimers;
-        private int frameCounter;
 
         public new WindowName WindowName => WindowName.Timers;
 
         private TimersWindowSettings Settings => Service.Configuration.Windows.Timers;
 
-        private const ImGuiWindowFlags DefaultFlags = ImGuiWindowFlags.NoFocusOnAppearing |
-                                                      ImGuiWindowFlags.NoTitleBar |
-                                                      ImGuiWindowFlags.NoScrollbar |
-                                                      ImGuiWindowFlags.NoCollapse;
-
-        private const ImGuiWindowFlags ClickThroughFlags = ImGuiWindowFlags.NoFocusOnAppearing |
-                                                           ImGuiWindowFlags.NoDecoration |
-                                                           ImGuiWindowFlags.NoInputs;
-
         public TimersWindow() : base("DailyDuty Timers")
         {
             Service.WindowSystem.AddWindow(this);
-
-            Service.Framework.Update += Update;
 
             var timersList = Service.TimerManager.GetTimers(WindowName.Timers);
             countdownTimers = new CountdownTimers(timersList);
         }
 
-        private void Update(Framework framework)
+        public override void PreOpenCheck()
         {
-            if (Service.LoggedIn == false)
-            {
-                IsOpen = false;
-                return;
-            }
+            IsOpen = Settings.Open;
+        }
 
-            if(frameCounter++ % 10 != 0) return;
-
+        public override bool DrawConditions()
+        {
             bool isInQuestEvent = Service.Condition[ConditionFlag.OccupiedInQuestEvent];
-
-            IsOpen = Settings.Open && !isInQuestEvent;
 
             if (Settings.HideInDuty == true)
             {
                 if (Utilities.Condition.IsBoundByDuty() == true)
                 {
-                    IsOpen = false;
+                    return false;
                 }
             }
 
-            Flags = Settings.ClickThrough ? ClickThroughFlags : DefaultFlags;
+            return !isInQuestEvent;
         }
 
         public override void PreDraw()
         {
-            var clr = ImGui.GetStyle().Colors[(int)ImGuiCol.WindowBg];
-            ImGui.PushStyleColor(ImGuiCol.WindowBg, new Vector4(clr.X, clr.Y, clr.Z, Settings.Opacity));
+            Flags = Settings.ClickThrough ? DrawFlags.ClickThroughFlags : DrawFlags.DefaultFlags;
 
-            clr = ImGui.GetStyle().Colors[(int)ImGuiCol.Border];
-            ImGui.PushStyleColor(ImGuiCol.Border, new Vector4(clr.X, clr.Y, clr.Z, Settings.Opacity));
+            var color = ImGui.GetStyle().Colors[(int)ImGuiCol.WindowBg];
+            ImGui.PushStyleColor(ImGuiCol.WindowBg, new Vector4(color.X, color.Y, color.Z, Settings.Opacity));
+
+            color = ImGui.GetStyle().Colors[(int)ImGuiCol.Border];
+            ImGui.PushStyleColor(ImGuiCol.Border, new Vector4(color.X, color.Y, color.Z, Settings.Opacity));
         }
 
         public override void Draw()
         {
+            if (IsOpen == false) return;
+
             countdownTimers.Draw();
         }
 
@@ -86,8 +74,6 @@ namespace DailyDuty.Windows.Timers
 
         public void Dispose()
         {
-            Service.Framework.Update -= Update;
-            
             Service.WindowSystem.RemoveWindow(this);
         }
     }
