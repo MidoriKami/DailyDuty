1cc9ecf557bc8706b13ca409b9bc2898389278be
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-03-12 23:24:06 -0800

    Define flags as const instead of static

--- DailyDuty/Utilities/DrawFlags.cs
+++ DailyDuty/Utilities/DrawFlags.cs
@@ -4,20 +4,22 @@ namespace DailyDuty.Utilities
 {
     internal static class DrawFlags
     {
-        public static ImGuiWindowFlags DefaultFlags = ImGuiWindowFlags.NoFocusOnAppearing |
-                                                       ImGuiWindowFlags.NoTitleBar |
-                                                       ImGuiWindowFlags.NoScrollbar |
-                                                       ImGuiWindowFlags.NoCollapse;
+        public const ImGuiWindowFlags DefaultFlags = ImGuiWindowFlags.NoFocusOnAppearing |
+                                                     ImGuiWindowFlags.NoTitleBar |
+                                                     ImGuiWindowFlags.NoScrollbar |
+                                                     ImGuiWindowFlags.NoCollapse;
 
-        public static ImGuiWindowFlags AutoResize = ImGuiWindowFlags.NoFocusOnAppearing |
-                                                    ImGuiWindowFlags.NoTitleBar |
-                                                    ImGuiWindowFlags.NoScrollbar |
-                                                    ImGuiWindowFlags.NoCollapse |
-                                                    ImGuiWindowFlags.AlwaysAutoResize;
+        public const ImGuiWindowFlags AutoResize = ImGuiWindowFlags.NoFocusOnAppearing |
+                                                   ImGuiWindowFlags.NoTitleBar |
+                                                   ImGuiWindowFlags.NoScrollbar |
+                                                   ImGuiWindowFlags.NoCollapse |
+                                                   ImGuiWindowFlags.AlwaysAutoResize;
 
         public const ImGuiWindowFlags ClickThroughFlags = ImGuiWindowFlags.NoFocusOnAppearing |
                                                           ImGuiWindowFlags.NoDecoration |
                                                           ImGuiWindowFlags.NoInputs |
                                                           ImGuiWindowFlags.AlwaysAutoResize;
+
+        public const ImGuiWindowFlags Debug = ImGuiWindowFlags.NoBringToFrontOnFocus;
     }
 }
