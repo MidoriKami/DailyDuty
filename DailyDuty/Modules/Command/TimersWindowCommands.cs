95d48734e720da64699aee875d51c4fa5d13fdd3
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-03-21 15:13:08 -0700

    Expanded commands for todo and timers windows

--- DailyDuty/Modules/Command/TimersWindowCommands.cs
+++ DailyDuty/Modules/Command/TimersWindowCommands.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DailyDuty.Data.Enums;
+using DailyDuty.Data.SettingsObjects.Windows;
 using DailyDuty.Interfaces;
 using DailyDuty.Windows.Notice;
 using DailyDuty.Windows.Timers;
@@ -12,6 +13,8 @@ namespace DailyDuty.Modules.Command
 {
     internal class TimersWindowCommands : ICommand
     {
+        private TimersWindowSettings Settings => Service.Configuration.Windows.Timers;
+
         List<string> ICommand.ModuleCommands { get; } = new()
         {
             "timers",
@@ -20,7 +23,20 @@ namespace DailyDuty.Modules.Command
 
         void ICommand.Execute(string primaryCommand, string? secondaryCommand)
         {
-            Service.Configuration.Windows.Timers.Open = !Service.Configuration.Windows.Timers.Open;
+            var lastState = Settings.Open;
+
+            Settings.Open = secondaryCommand switch
+            {
+                null => !Settings.Open,
+                "on" or "yes" or "show" or "enable" => true,
+                "off" or "no" or "hide" or "disable" => false,
+                _ => Settings.Open
+            };
+
+            if (lastState != Settings.Open)
+            {
+                Service.Configuration.Save();
+            }
         }
     }
 }
--- DailyDuty/Modules/Command/TodoWindowCommands.cs
+++ DailyDuty/Modules/Command/TodoWindowCommands.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DailyDuty.Data.SettingsObjects.Windows;
 using DailyDuty.Interfaces;
 
 namespace DailyDuty.Modules.Command
 {
     internal class TodoWindowCommands : ICommand
     {
+        private TodoWindowSettings Settings => Service.Configuration.Windows.Todo;
+
         List<string> ICommand.ModuleCommands { get; } = new()
         {
             "todo"
@@ -16,7 +19,20 @@ namespace DailyDuty.Modules.Command
 
         void ICommand.Execute(string primaryCommand, string? secondaryCommand)
         {
-            Service.Configuration.Windows.Todo.Open = !Service.Configuration.Windows.Todo.Open;
+            var lastState = Settings.Open;
+
+            Settings.Open = secondaryCommand switch
+            {
+                null => !Settings.Open,
+                "on" or "yes" or "show" or "enable" => true,
+                "off" or "no" or "hide" or "disable" => false,
+                _ => Settings.Open
+            };
+
+            if (lastState != Settings.Open)
+            {
+                Service.Configuration.Save();
+            }
         }
     }
 }
