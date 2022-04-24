d4618d7ac6da0de6040a62777bd4aa19dca4b3f1
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-04-23 23:39:48 -0700

    New Tab Items, LogDebug, MainWindowSettings, NotificationSettings.

---
+++ DailyDuty/Graphical/TabItems/MainWindowSettingsTabItem.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DailyDuty.Data.Components;
+using DailyDuty.Enums;
+using DailyDuty.Interfaces;
+using DailyDuty.Localization;
+using Dalamud.Interface;
+using ImGuiNET;
+
+namespace DailyDuty.Graphical.TabItems
+{
+    internal class MainWindowSettingsTabItem : ITabItem
+    {
+        public ModuleType ModuleType => ModuleType.MainWindowSettings;
+        private static MainWindowSettings Settings => Service.SystemConfiguration.Windows.MainWindow;
+
+
+        private readonly InfoBox style = new()
+        {
+            Label = Strings.Common.StyleOptionsLabel,
+            ContentsAction = () =>
+            {
+                ImGui.PushItemWidth(175 * ImGuiHelpers.GlobalScale);
+                ImGui.DragFloat(Strings.Common.OpacityLabel, ref Settings.Opacity, 0.01f, 0.0f, 1.0f);
+                ImGui.PopItemWidth();
+
+                if (ImGui.Button(Strings.Common.SaveLabel, ImGuiHelpers.ScaledVector2(75.0f, 23.0f)))
+                {
+                    Service.SystemConfiguration.Save();
+                }
+            }
+        };
+
+        public void DrawTabItem()
+        {
+            ImGui.Text(Strings.Configuration.MainWindowConfigurationLabel);
+        }
+
+        public void DrawConfigurationPane()
+        {
+            ImGuiHelpers.ScaledDummy(10.0f);
+            style.DrawCentered(0.80f);
+            
+            //ImGuiHelpers.ScaledDummy(30.0f);
+
+            ImGuiHelpers.ScaledDummy(20.0f);
+        }
+    }
+}
---
+++ DailyDuty/Graphical/TabItems/NotificationSettingsTabItem.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DailyDuty.Data.Components;
+using DailyDuty.Enums;
+using DailyDuty.Interfaces;
+using DailyDuty.Localization;
+using Dalamud.Interface;
+using ImGuiNET;
+
+namespace DailyDuty.Graphical.TabItems
+{
+    internal class NotificationSettingsTabItem : ITabItem
+    {
+        public ModuleType ModuleType => ModuleType.NotificationSettings;
+
+        private static SystemSettings Settings => Service.SystemConfiguration.System;
+
+        private readonly InfoBox notificationThrottle = new InfoBox()
+        {
+            Label = Strings.Configuration.NotificationsThrottleLabel,
+            ContentsAction = () =>
+            {
+                ImGui.Text(Strings.Configuration.NotificationsThrottleDescription);
+                
+                ImGuiHelpers.ScaledDummy(10.0f);
+
+                ImGui.PushItemWidth(50 * ImGuiHelpers.GlobalScale);
+                ImGui.InputInt("", ref Settings.MinutesBetweenThrottledMessages, 0, 0);
+
+                ImGui.SameLine();
+                ImGui.Text(Strings.Common.MinutesLabel);
+            }
+        };
+
+        public void DrawTabItem()
+        {
+            ImGui.Text(Strings.Configuration.NotificationsConfigurationLabel);
+        }
+
+        public void DrawConfigurationPane()
+        {
+            ImGuiHelpers.ScaledDummy(10.0f);
+            notificationThrottle.DrawCentered();
+            
+            //ImGuiHelpers.ScaledDummy(30.0f);
+
+            ImGuiHelpers.ScaledDummy(20.0f);
+        }
+    }
+}
