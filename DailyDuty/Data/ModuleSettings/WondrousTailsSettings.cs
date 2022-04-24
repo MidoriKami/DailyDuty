6a26497a2b2592e3e8327a7ec92b90f5d278f06e
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-04-24 15:20:24 -0700

    Remove obsolete settings options

--- DailyDuty/Data/ModuleSettings/WondrousTailsSettings.cs
+++ DailyDuty/Data/ModuleSettings/WondrousTailsSettings.cs
@@ -5,12 +5,10 @@ namespace DailyDuty.Data.ModuleSettings
 {
     public class WondrousTailsSettings : GenericSettings
     {
-        public int NumPlacedStickers = 0;
         public bool InstanceNotifications = false;
         public bool RerollNotificationStickers = false;
         public bool RerollNotificationTasks = false;
         public bool NewBookNotification = false;
-        public DateTime CompletionDate = new();
         public bool BookCompleteNotification = false;
     }
 }
