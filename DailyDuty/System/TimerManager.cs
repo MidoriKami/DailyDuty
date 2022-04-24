32d7e9d53c75a07e3a1105fd102e9aef35d580ae
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-04-23 21:41:09 -0700

    Add Timers Framework

---
+++ DailyDuty/System/TimerManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using DailyDuty.Localization;
+using DailyDuty.Timers;
+using DailyDuty.Utilities;
+
+namespace DailyDuty.System
+{
+    public class TimerManager : IDisposable
+    {
+        public readonly List<CountdownTimer> Timers = new()
+        {
+            new CountdownTimer()
+            {
+                Label = Strings.Timers.DailyResetLabel,
+                Period = TimeSpan.FromDays(1),
+                UpdateNextReset = Time.NextDailyReset
+            },
+            new CountdownTimer()
+            {
+                Label = Strings.Timers.FashionReportLabel,
+                Period = TimeSpan.FromDays(7),
+                UpdateNextReset = () =>
+                {
+                    var now = DateTime.UtcNow;
+
+                    var fashionReportOpen = Time.NextFashionReportReset();
+                    var fashionReportClose = Time.NextWeeklyReset();
+
+                    if (now > fashionReportOpen && now < fashionReportClose)
+                    {
+                        return Time.NextWeeklyReset().AddDays(3);
+                    }
+                    else
+                    {
+                        return Time.NextFashionReportReset();
+                    }
+                }
+            },
+            new CountdownTimer()
+            {
+                Label = Strings.Timers.JumboCactpotLabel,
+                Period = TimeSpan.FromDays(7),
+                UpdateNextReset = Time.NextJumboCactpotReset
+            },
+            new CountdownTimer()
+            {
+                Label = Strings.Timers.LeveAllowanceLabel,
+                Period = TimeSpan.FromDays(1),
+                UpdateNextReset = Time.NextLeveAllowanceReset
+            },
+            //new CountdownTimer()
+            //{
+            //    Label = Strings.Timers.TreasureMapLabel,
+            //    Period = TimeSpan.FromHours(18),
+            //    UpdateNextReset = Time.
+            //},
+            new CountdownTimer()
+            {
+                Label = Strings.Timers.WeeklyResetLabel,
+                Period = TimeSpan.FromDays(7),
+                UpdateNextReset = Time.NextWeeklyReset
+            }
+        };
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ DailyDuty/Timers/CountdownTimer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Numerics;
+using DailyDuty.Data.Components;
+using DailyDuty.Interfaces;
+using Dalamud.Interface;
+using ImGuiNET;
+
+namespace DailyDuty.Timers
+{
+    public class CountdownTimer : IDrawable
+    {
+        private static MainWindowSettings Settings => Service.SystemConfiguration.Windows.MainWindow;
+        public string Label { get; init; } = "Label Not Set";
+        public TimeSpan Period { get; init; }
+        public Func<DateTime>? UpdateNextReset { get; init; }
+        private DateTime NextReset { get; set; }
+
+        public void Draw()
+        {
+            Update();
+
+            ImGui.PushID(Label);
+
+            var contentWidth = ImGui.GetContentRegionAvail();
+            var textWidth = contentWidth.X * 0.30f;
+            var progressBarWidth = contentWidth * 0.50f;
+            var textSize = ImGui.CalcTextSize(Label);
+
+            ImGui.Text(Label);
+
+            ImGui.SameLine(textWidth);
+            var cursorStartPosition = ImGui.GetCursorPos();
+
+            var remainingTime = NextReset - DateTime.UtcNow;
+            var deltaTime = 1.0f - (float)(remainingTime / Period);
+            ImGui.ProgressBar(deltaTime, progressBarWidth with {Y = textSize.Y}, "");
+
+            textSize = ImGui.CalcTextSize(FormatTimeSpan(remainingTime, Settings.HideSeconds));
+            var textStartX = contentWidth.X - textSize.X - 20.0f * ImGuiHelpers.GlobalScale;
+
+            ImGui.SetCursorPos(new Vector2(textStartX, cursorStartPosition.Y));
+
+            ImGui.Text(FormatTimeSpan(remainingTime, Settings.HideSeconds));
+
+            ImGui.PopID();
+        }
+
+        private void Update()
+        {
+            if (DateTime.UtcNow >= NextReset)
+            {
+                if (UpdateNextReset == null)
+                {
+                    throw new NotImplementedException();
+                }
+
+                NextReset = UpdateNextReset();
+            }
+        }
+
+        private static string FormatTimeSpan(TimeSpan span, bool hideSeconds = false)
+        {
+            var baseString = span.Days > 0 ? $"{span.Days:0}:{span.Hours:00}:{span.Minutes:00}" : $"{span.Hours:00}:{span.Minutes:00}";
+
+            if (hideSeconds == false)
+            {
+                return baseString + $":{span.Seconds:00}";
+            }
+            else
+            {
+                return baseString;
+            }
+        }
+    }
+}
