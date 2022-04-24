4d11ad790e1b4be4c41259be3a4f1aa662575846
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-04-24 12:51:25 -0700

    Directives cleanup

--- DailyDuty/Graphical/TabItems/StyleTabItem.cs
+++ DailyDuty/Graphical/TabItems/StyleTabItem.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DailyDuty.Data.Components;
+﻿using DailyDuty.Data.Components;
 using DailyDuty.Enums;
 using DailyDuty.Interfaces;
 using DailyDuty.Localization;
