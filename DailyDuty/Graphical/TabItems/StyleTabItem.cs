fd1c5a26cd8e81e08c50ca7e5c03c1fe8c253a81
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-04-25 13:43:33 -0700

    Auto save after a halfsecond has elapsed since the last slider movement

--- DailyDuty/Graphical/TabItems/StyleTabItem.cs
+++ DailyDuty/Graphical/TabItems/StyleTabItem.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using DailyDuty.Data.Components;
+﻿using DailyDuty.Data.Components;
 using DailyDuty.Enums;
 using DailyDuty.Interfaces;
 using DailyDuty.Localization;
@@ -13,7 +12,6 @@ namespace DailyDuty.Graphical.TabItems
         public ModuleType ModuleType => ModuleType.MainWindowSettings;
         private static MainWindowSettings Settings => Service.SystemConfiguration.Windows.MainWindow;
 
-        private static readonly Stopwatch SettingsStopwatch = new();
 
         private readonly InfoBox style = new()
         {
@@ -21,16 +19,12 @@ namespace DailyDuty.Graphical.TabItems
             ContentsAction = () =>
             {
                 ImGui.PushItemWidth(175 * ImGuiHelpers.GlobalScale);
-                if (ImGui.DragFloat(Strings.Common.OpacityLabel, ref Settings.Opacity, 0.01f, 0.0f, 1.0f))
-                {
-                    SettingsStopwatch.Restart();
-                }
+                ImGui.DragFloat(Strings.Common.OpacityLabel, ref Settings.Opacity, 0.01f, 0.0f, 1.0f);
                 ImGui.PopItemWidth();
 
-                if (SettingsStopwatch.ElapsedMilliseconds > 500)
+                if (ImGui.Button(Strings.Common.SaveLabel, ImGuiHelpers.ScaledVector2(75.0f, 23.0f)))
                 {
                     Service.SystemConfiguration.Save();
-                    SettingsStopwatch.Reset();
                 }
             }
         };
