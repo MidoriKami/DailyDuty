69cf4dc5d150acf99ba766800a493244e052c7d8
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-04-28 22:19:23 -0700

    Add auto-refresh to Jumbo Cactpot

--- DailyDuty/Modules/JumboCactpotModule.cs
+++ DailyDuty/Modules/JumboCactpotModule.cs
@@ -9,6 +9,7 @@ using Dalamud.Game.Text.SeStringHandling.Payloads;
 using Dalamud.Hooking;
 using Dalamud.Utility.Signatures;
 using FFXIVClientStructs.FFXIV.Component.GUI;
+using Condition = DailyDuty.Utilities.Condition;
 
 namespace DailyDuty.Modules
 {
@@ -28,9 +29,41 @@ namespace DailyDuty.Modules
 
         private delegate void* AddonReceiveEvent(AgentInterface* addon, void* a2, AtkValue* eventData, int eventDataItemCount, int senderID);
 
-            // LotteryWeekly_ReceiveEvent
         [Signature("48 89 5C 24 ?? 44 89 4C 24 ?? 4C 89 44 24 ?? 55 56 57 41 54 41 55 41 56 41 57 48 8D 6C 24", DetourName = nameof(LotteryWeekly_ReceiveEvent))]
         private readonly Hook<AddonReceiveEvent>? receiveEventHook = null;
+
+        private delegate void* GoldSaucerUpdateDelegate(void* a1, byte* a2, uint a3, ushort a4, void* a5, int* a6, byte a7);
+
+        [Signature("E8 ?? ?? ?? ?? 80 A7 ?? ?? ?? ?? ?? 48 8D 8F ?? ?? ?? ?? 44 89 AF", DetourName = nameof(GoldSaucerUpdate))]
+        private readonly Hook<GoldSaucerUpdateDelegate>? goldSaucerUpdateHook = null;
+
+        private void* GoldSaucerUpdate(void* a1, byte* a2, uint a3, ushort a4, void* a5, int* a6,  byte a7)
+        {
+            var result = goldSaucerUpdateHook!.Original(a1, a2, a3, a4, a5, a6, a7);
+
+            //1010445 Mini Cactpot Broker
+            //1010446 Jumbo Cactpot Broker
+
+            if (Service.TargetManager.Target?.DataId == 1010446)
+            {
+                //a6[2,3,4]
+                for(var i = 0; i < 3; ++i)
+                {
+                    var ticketValue = a6[i + 2];
+
+                    if (ticketValue != 10000)
+                    {
+                        if (!Settings.Tickets.Contains(ticketValue))
+                        {
+                            Settings.Tickets.Add(ticketValue);
+                            Service.CharacterConfiguration.Save();
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
 
         private static JumboCactpotSettings Settings => Service.CharacterConfiguration.JumboCactpot;
         private int ticketData = -1;
@@ -42,11 +75,13 @@ namespace DailyDuty.Modules
             goldSaucerTeleport = Service.TeleportManager.GetPayload(ChatPayloads.GoldSaucerTeleport);
 
             receiveEventHook?.Enable();
+            goldSaucerUpdateHook?.Enable();
         }
 
         public void Dispose()
         {
             receiveEventHook?.Dispose();
+            goldSaucerUpdateHook?.Dispose();
         }
 
         public void SendNotification()
