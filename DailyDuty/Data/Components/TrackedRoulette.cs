1a15d13096cc820b7e7a799d6de193eb076f2e30
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-05-02 16:38:23 -0700

    Use properties

--- DailyDuty/Data/Components/TrackedRoulette.cs
+++ DailyDuty/Data/Components/TrackedRoulette.cs
@@ -4,13 +4,8 @@ namespace DailyDuty.Data.Components
 {
     public class TrackedRoulette
     {
-        public RouletteType Type { get; }
+        public RouletteType Type { get; init; }
         public bool Tracked = false;
         public bool Completed = false;
-
-        public TrackedRoulette(RouletteType type)
-        {
-            Type = type;
-        }
     }
 }
