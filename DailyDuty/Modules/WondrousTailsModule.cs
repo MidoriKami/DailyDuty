110955b74f93fd86c0f7f06ee419253e07a032fd
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2022-06-03 18:29:20 -0700

    Add Enabled Check to Duty Started Listener

--- DailyDuty/Modules/WondrousTailsModule.cs
+++ DailyDuty/Modules/WondrousTailsModule.cs
@@ -69,31 +69,34 @@ namespace DailyDuty.Modules
 
         private byte DutyEventFunction(void* a1, void* a2, ushort* a3)
         {
-            var category = *(a3);
-            var type = *(uint*)(a3 + 4);
-
-            // DirectorUpdate Category
-            if (category == 0x6D)
+            if (Settings.Enabled)
             {
-                switch (type)
+                var category = *(a3);
+                var type = *(uint*)(a3 + 4);
+
+                // DirectorUpdate Category
+                if (category == 0x6D)
                 {
-                    // Duty Commenced
-                    case 0x40000001 when Settings.InstanceNotifications && !IsCompleted():
-                        OnDutyStartNotification(Service.ClientState.TerritoryType);
-                        break;
+                    switch (type)
+                    {
+                        // Duty Commenced
+                        case 0x40000001 when Settings.InstanceNotifications && !IsCompleted():
+                            OnDutyStartNotification(Service.ClientState.TerritoryType);
+                            break;
 
-                    // Party Wipe
-                    case 0x40000005:
-                        break;
+                        // Party Wipe
+                        case 0x40000005:
+                            break;
 
-                    // Duty Recommence
-                    case 0x40000006:
-                        break;
+                        // Duty Recommence
+                        case 0x40000006:
+                            break;
 
-                    // Duty Completed
-                    case 0x40000003 when Settings.InstanceNotifications && !IsCompleted():
-                        OnDutyEndNotification(Service.ClientState.TerritoryType);
-                        break;
+                        // Duty Completed
+                        case 0x40000003 when Settings.InstanceNotifications && !IsCompleted():
+                            OnDutyEndNotification(Service.ClientState.TerritoryType);
+                            break;
+                    }
                 }
             }
 
