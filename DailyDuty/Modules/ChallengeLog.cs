1837456a2e919d104727cd9058b1fc39e4602076
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-01-24 10:03:35 -0800

    Remove Manual Challenge Log Tracking

--- DailyDuty/Modules/ChallengeLog.cs
+++ DailyDuty/Modules/ChallengeLog.cs
@@ -1,32 +1,22 @@
 ﻿using System;
 using DailyDuty.Addons;
 using DailyDuty.DataModels;
-using DailyDuty.DataStructures;
 using DailyDuty.Interfaces;
 using DailyDuty.Localization;
 using DailyDuty.UserInterface.Components;
 using DailyDuty.Utilities;
 using Dalamud.Game.Text.SeStringHandling.Payloads;
-using Dalamud.Hooking;
-using Dalamud.Logging;
-using Dalamud.Utility.Signatures;
-using KamiLib.Caching;
+using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using KamiLib.ChatCommands;
 using KamiLib.Configuration;
 using KamiLib.Drawing;
-using KamiLib.GameState;
 using KamiLib.Interfaces;
 using KamiLib.Misc;
-using Lumina.Excel.GeneratedSheets;
 
 namespace DailyDuty.Modules;
 
 internal class ChallengeLogSettings : GenericSettings
 {
-    public int Commendations;
-    public int RouletteDungeons;
-    public int DungeonMaster;
-
     public Setting<bool> CommendationWarning = new(true);
     public Setting<bool> RouletteDungeonWarning = new(true);
     public Setting<bool> DungeonWarning = new(true);
@@ -95,20 +85,24 @@ internal class ChallengeLog : IModule
         {
             InfoBox.Instance.DrawGenericStatus(this);
 
+            var commendationStatus = ModuleLogicComponent.CommendationStatus() == ModuleStatus.Complete;
+            var rouletteStatus = ModuleLogicComponent.DungeonRouletteStatus() == ModuleStatus.Complete;
+            var dungeonMasterStatus = ModuleLogicComponent.DungeonMasterStatus() == ModuleStatus.Complete;
+            
             InfoBox.Instance
                 .AddTitle(Strings.Common_Battle)
                 .BeginTable()
                 .BeginRow()
                 .AddString(Strings.ChallengeLog_Commendation)
-                .AddString($"{Settings.Commendations} / 5", ModuleLogicComponent.CommendationStatus().GetStatusColor())
+                .AddString(commendationStatus ? Strings.Common_Complete : Strings.Common_Incomplete, commendationStatus ? Colors.Green : Colors.Orange)
                 .EndRow()
                 .BeginRow()
                 .AddString(Strings.ChallengeLog_DungeonRoulette)
-                .AddString($"{Settings.RouletteDungeons} / 3", ModuleLogicComponent.DungeonRouletteStatus().GetStatusColor())
+                .AddString(rouletteStatus ? Strings.Common_Complete : Strings.Common_Incomplete, rouletteStatus ? Colors.Green : Colors.Orange)
                 .EndRow()
                 .BeginRow()
                 .AddString(Strings.ChallengeLog_DungeonMaster)
-                .AddString($"{Settings.DungeonMaster} / 5", ModuleLogicComponent.DungeonMasterStatus().GetStatusColor())
+                .AddString(dungeonMasterStatus ? Strings.Common_Complete : Strings.Common_Incomplete, dungeonMasterStatus ? Colors.Green : Colors.Orange)
                 .EndRow()
                 .EndTable()
                 .Draw();
@@ -122,55 +116,21 @@ internal class ChallengeLog : IModule
         public IModule ParentModule { get; }
         public DalamudLinkPayload? DalamudLinkPayload => null;
         public bool LinkPayloadActive => false;
-
-        private delegate void* ChallengeLogNetworkData(void* a1, int a2, int* a3);
-        [Signature("40 55 57 41 56 48 83 EC 20 0F B6 41 1C", DetourName = nameof(ProcessNetworkPacket))]
-        private readonly Hook<ChallengeLogNetworkData>? refreshChallengeLogHook = null;
-
-        private enum DungeonDutyRouletteState
-        {
-            NotDungeonDuty,
-            DutyPopped,
-            DutyCommencing,
-            EnteredDuty,
-            DutyCompleted
-        }
-
-        private DungeonDutyRouletteState dutyState = DungeonDutyRouletteState.NotDungeonDuty;
-
+        
         public ModuleLogicComponent(IModule parentModule)
         {
             ParentModule = parentModule;
-            SignatureHelper.Initialise(this);
 
-            refreshChallengeLogHook?.Enable();
-
-            Service.ClientState.CfPop += OnContentFinderPop;
-            Service.ClientState.TerritoryChanged += OnTerritoryChanged;
-
-            CommendationAddon.Instance.ReceiveEvent += CommendationOnReceiveEvent;
             CommendationAddon.Instance.Show += CommendationOnShow;
             
-            DutyFinderAddon.Instance.ReceiveEvent += DutyFinderOnReceiveEvent;
             DutyFinderAddon.Instance.Show += DutyFinderOnShow;
-
-            DutyState.Instance.DutyCompleted += OnDutyCompleted;
         }
 
         public void Dispose()
         {
-            refreshChallengeLogHook?.Dispose();
-
-            Service.ClientState.CfPop -= OnContentFinderPop;
-            Service.ClientState.TerritoryChanged -= OnTerritoryChanged;
-
-            CommendationAddon.Instance.ReceiveEvent -= CommendationOnReceiveEvent;
             CommendationAddon.Instance.Show -= CommendationOnShow;
             
-            DutyFinderAddon.Instance.ReceiveEvent -= DutyFinderOnReceiveEvent;
             DutyFinderAddon.Instance.Show -= DutyFinderOnShow;
-
-            DutyState.Instance.DutyCompleted -= OnDutyCompleted;
         }
 
         private void DutyFinderOnShow(object? sender, nint e)
@@ -178,79 +138,14 @@ internal class ChallengeLog : IModule
             if (!Settings.Enabled) return;
             if (Settings.Suppressed) return;
 
-            if (Settings.RouletteDungeonWarning && Settings.RouletteDungeons < 3)
+            if (Settings.RouletteDungeonWarning && DungeonRouletteStatus() != ModuleStatus.Complete)
             {
-                Chat.Print(Strings.ChallengeLog_Label, $"{3 - Settings.RouletteDungeons} {Strings.ChallengeLog_DungeonRoulettesRemaining}");
+                Chat.Print(Strings.ChallengeLog_Label, $"{Strings.ChallengeLog_DungeonRoulette} {Strings.Common_AllowancesAvailable}");
             }
 
-            if (Settings.DungeonWarning && Settings.DungeonMaster < 5)
+            if (Settings.DungeonWarning && DungeonMasterStatus() != ModuleStatus.Complete)
             {
-                Chat.Print(Strings.ChallengeLog_Label, $"{5 - Settings.DungeonMaster} {Strings.ChallengeLog_DungeonMasterRemaining}");
-            }
-        }
-
-        private void OnContentFinderPop(object? sender, ContentFinderCondition queuedDuty) => dutyState = queuedDuty.RowId == 0 ? DungeonDutyRouletteState.DutyPopped : DungeonDutyRouletteState.NotDungeonDuty;
-
-        private void DutyFinderOnReceiveEvent(object? sender, ReceiveEventArgs e)
-        {
-            // Commence / Wait / Withdraw dialogue
-            if (e.SenderID != 2) return;
-
-            // 8 = Commence button was pushed
-            if (e.EventArgs[0].Int == 8 && dutyState == DungeonDutyRouletteState.DutyPopped)
-            {
-                dutyState = DungeonDutyRouletteState.DutyCommencing;
-            }
-        }
-
-        private void OnDutyCompleted(uint territory)
-        {
-            if (dutyState == DungeonDutyRouletteState.EnteredDuty)
-            {
-                dutyState = DungeonDutyRouletteState.DutyCompleted;
-            }
-
-            if (GetDutyType(Service.ClientState.TerritoryType) == 2 && Settings.DungeonMaster < 5)
-            {
-                Settings.DungeonMaster += 1;
-                Service.ConfigurationManager.Save();
-            }
-        }
-        
-        private void OnTerritoryChanged(object? sender, ushort e)
-        {
-            switch (dutyState)
-            {
-                case DungeonDutyRouletteState.DutyCommencing:
-                    // 2 = Dungeons
-                    if (GetDutyType(e) == 2)
-                    {
-                        dutyState = DungeonDutyRouletteState.EnteredDuty;
-                    }
-                    break;
-
-                case DungeonDutyRouletteState.DutyCompleted:
-                    if (Settings.RouletteDungeons < 3)
-                    {
-                        Settings.RouletteDungeons += 1;
-                        Service.ConfigurationManager.Save();
-                    }
-                    
-                    dutyState = DungeonDutyRouletteState.NotDungeonDuty;
-                    break;
-
-                default:
-                    dutyState = DungeonDutyRouletteState.NotDungeonDuty;
-                    break;
-            }
-        }
-
-        private void CommendationOnReceiveEvent(object? sender, ReceiveEventArgs e)
-        {
-            if (e.EventArgsCount == 2 && Settings.Commendations < 5)
-            {
-                Settings.Commendations += 1;
-                Service.ConfigurationManager.Save();
+                Chat.Print(Strings.ChallengeLog_Label, $"{Strings.ChallengeLog_DungeonMaster} {Strings.Common_AllowancesAvailable}");
             }
         }
 
@@ -259,53 +154,19 @@ internal class ChallengeLog : IModule
             if (!Settings.Enabled) return;
             if (Settings.Suppressed) return;
 
-            if (Settings.CommendationWarning && Settings.Commendations < 5)
+            if (Settings.CommendationWarning && CommendationStatus() != ModuleStatus.Complete)
             {
-                Chat.Print(Strings.ChallengeLog_Label, $"{5 - Settings.Commendations} {Strings.ChallengeLog_CommendationsRemaining}");
+                Chat.Print(Strings.ChallengeLog_Label, $"{Strings.ChallengeLog_Commendation} {Strings.Common_AllowancesAvailable}");
             }
         }
-
-        private void* ProcessNetworkPacket(void* a1, int tab, int* a3)
-        {
-            try
-            {
-                switch (tab)
-                {
-                    // Battle Tab
-                    case 1:
-                        var data = (ChallengeLogStruct.Battles*) a3;
-                        Settings.Commendations = data->Commendations;
-                        Settings.RouletteDungeons = data->DungeonRoulette;
-                        Settings.DungeonMaster = data->DungeonMaster;
-                        Service.ConfigurationManager.Save();
-                        break;
-                }
-            }
-            catch (Exception e)
-            {
-                PluginLog.Error(e, "Unable to Refresh Challenge Log Data");
-            }
-
-            return refreshChallengeLogHook!.Original(a1, tab, a3);
-        }
-
-        private static uint? GetDutyType(uint territoryType)
-        {
-            var territoryInfo = LuminaCache<TerritoryType>.Instance.GetRow(territoryType);
-            var contentFinderInfo = territoryInfo?.ContentFinderCondition.Value;
-            var contentType = contentFinderInfo?.ContentType.Value;
-
-            return contentType?.RowId;
-        }
-
+        
         public string GetStatusMessage() => string.Empty;
 
         public DateTime GetNextReset() => Time.NextWeeklyReset();
 
         public void DoReset()
         {
-            Settings.Commendations = 0;
-            Settings.RouletteDungeons = 0;
+            // Nothing to do here
         }
 
         public ModuleStatus GetModuleStatus()
@@ -317,26 +178,9 @@ internal class ChallengeLog : IModule
             return ModuleStatus.Complete;
         }
 
-        public static ModuleStatus CommendationStatus()
-        {
-            if (Settings.CommendationWarning && Settings.Commendations < 5) return ModuleStatus.Incomplete;
-
-            return ModuleStatus.Complete;
-        }
-
-        public static ModuleStatus DungeonRouletteStatus()
-        {
-            if (Settings.RouletteDungeonWarning && Settings.RouletteDungeons < 3) return ModuleStatus.Incomplete;
-
-            return ModuleStatus.Complete;
-        }
-
-        public static ModuleStatus DungeonMasterStatus()
-        {
-            if (Settings.DungeonWarning && Settings.DungeonMaster < 5) return ModuleStatus.Incomplete;
-
-            return ModuleStatus.Complete;
-        }
+        public static ModuleStatus CommendationStatus() => ContentsNote.Instance()->IsContentNoteComplete(25) ? ModuleStatus.Complete : ModuleStatus.Incomplete;
+        public static ModuleStatus DungeonRouletteStatus() => ContentsNote.Instance()->IsContentNoteComplete(1) ? ModuleStatus.Complete : ModuleStatus.Incomplete;
+        public static ModuleStatus DungeonMasterStatus()=> ContentsNote.Instance()->IsContentNoteComplete(2) ? ModuleStatus.Complete : ModuleStatus.Incomplete;
     }
 
     private class ModuleTodoComponent : ITodoComponent
