5f31228adeddd944a8df4438d550b24bb26fda13
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-03-21 14:58:02 -0700

    Remove unused ColorInfo class

--- DailyDuty/Views/Components/UiColorPickerView.cs
+++ DailyDuty/Views/Components/UiColorPickerView.cs
@@ -1,50 +1,22 @@
-﻿using System.Collections.Generic;
-using System.Numerics;
+﻿using System.Numerics;
 using ImGuiNET;
 using KamiLib.Caching;
 using Lumina.Excel.GeneratedSheets;
 
 namespace DailyDuty.Views.Components;
 
-public class ColorInfo
-{
-    public ushort Index = ushort.MaxValue;
-    public byte R;
-    public byte G;
-    public byte B;
-    public byte A;
-
-    public static ColorInfo FromUiColor(ushort index, uint foreground)
-    {
-        return new ColorInfo
-        {
-            Index = index,
-            R = (byte)((foreground >> 24) & 0xFF),
-            G = (byte)((foreground >> 16) & 0xFF),
-            B = (byte)((foreground >> 8) & 0xFF),
-            A = (byte)((foreground >> 0) & 0xFF),
-        };
-    }
-
-    public override string ToString()
-    {
-        return $"#{R:X2}{G:X2}{B:X2}:{Index}";
-    }
-
-    public Vector4 Vec4 => new(R / 255f, G / 255f, B / 255f, A / 255f);
-}
-
 public class UiColorPickerView
 {
-    private static bool ColorSelector(ref ushort index)
+    private static void ColorSelector(ref ushort index)
     {
         const ImGuiColorEditFlags paletteButtonFlags = ImGuiColorEditFlags.AlphaPreview | ImGuiColorEditFlags.NoPicker | ImGuiColorEditFlags.NoTooltip;
         
         ImGui.BeginGroup(); // Lock X position
         ImGui.Text("Current color:");
         ImGui.SameLine();
+        
         var selectedColor = ConvertToVector4(LuminaCache<UIColor>.Instance.GetRow(index)!.UIGlow);
-        ImGui.ColorButton($"SelectedColor", selectedColor);
+        ImGui.ColorButton("SelectedColor", selectedColor);
         var shouldClose = false;
 
         var counter = 0;
@@ -57,7 +29,7 @@ public class UiColorPickerView
             }
 
             var loopColor = ConvertToVector4(color.UIGlow);
-            if (ImGui.ColorButton($"##palette", loopColor, paletteButtonFlags, new Vector2(20, 20)))
+            if (ImGui.ColorButton("##palette", loopColor, paletteButtonFlags, new Vector2(20, 20)))
             {
                 index = (ushort) color.RowId;
                 shouldClose = true;
@@ -79,24 +51,21 @@ public class UiColorPickerView
         {
             ImGui.CloseCurrentPopup();
         }
-        return shouldClose;
     }
 
-    public static bool SelectorButton(ref ushort index)
+    public static void SelectorButton(ref ushort index)
     {
-        var colorSelected = false;
         var selectedColor = ConvertToVector4(LuminaCache<UIColor>.Instance.GetRow(index)!.UIGlow);
-        if (ImGui.ColorButton($"##pickerButton", selectedColor))
+        if (ImGui.ColorButton("##pickerButton", selectedColor))
         {
-            ImGui.OpenPopup($"popup");
+            ImGui.OpenPopup("colorPickerPopup");
         }
 
-        if (ImGui.BeginPopup($"popup"))
+        if (ImGui.BeginPopup("colorPickerPopup"))
         {
-            colorSelected = ColorSelector(ref index);
+            ColorSelector(ref index);
             ImGui.EndPopup();
         }
-        return colorSelected;
     }
     
     private static Vector4 ConvertToVector4(uint color)
