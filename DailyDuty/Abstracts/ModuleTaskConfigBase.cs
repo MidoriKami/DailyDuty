1b0b97aa3592f62e0db8bdfff34032a995fde8f4
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-03 00:47:10 -0700

    Add new Task Base Modules

---
+++ DailyDuty/Abstracts/ModuleTaskConfigBase.cs
@@ -0,0 +1,11 @@
+﻿using DailyDuty.Models;
+using DailyDuty.Models.Attributes;
+using Lumina.Excel;
+
+namespace DailyDuty.Abstracts;
+
+public class ModuleTaskConfigBase<T> : ModuleConfigBase where T : ExcelRow
+{
+    [SelectableTasks]
+    public LuminaTaskConfigList<T> TaskConfig = new();
+}
---
+++ DailyDuty/Abstracts/ModuleTaskDataBase.cs
@@ -0,0 +1,11 @@
+﻿using DailyDuty.Models;
+using DailyDuty.Models.Attributes;
+using Lumina.Excel;
+
+namespace DailyDuty.Abstracts;
+
+public class ModuleTaskDataBase<T> : ModuleDataBase where T : ExcelRow
+{
+    [SelectableTasks] 
+    public LuminaTaskDataList<T> TaskData = new();
+}
