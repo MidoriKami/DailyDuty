bae1d9f157d9b8c8b4b6d66b4af008228bf4d6ec
MidoriKami <9083275+MidoriKami@users.noreply.github.com>
2023-06-09 00:40:09 -0700

    Undo Rename to Prevent stuff from breaking

--- DailyDuty/Models/LuminaTaskDataList.cs
+++ DailyDuty/Models/LuminaTaskDataList.cs
@@ -15,17 +15,17 @@ namespace DailyDuty.Models;
 
 public class LuminaTaskDataList<T> : IDrawable, ICollection<LuminaTaskData<T>> where T : ExcelRow
 {
-    private readonly List<LuminaTaskData<T>> dataList = new();
+    private readonly List<LuminaTaskData<T>> DataList = new();
 
     // Implement ICollection
-    public IEnumerator<LuminaTaskData<T>> GetEnumerator() => dataList.GetEnumerator();
+    public IEnumerator<LuminaTaskData<T>> GetEnumerator() => DataList.GetEnumerator();
     IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-    public void Add(LuminaTaskData<T> item) => dataList.Add(item);
-    public void Clear() => dataList.Clear();
-    public bool Contains(LuminaTaskData<T> item) => dataList.Contains(item);
-    public void CopyTo(LuminaTaskData<T>[] array, int arrayIndex) => dataList.CopyTo(array, arrayIndex);
-    public bool Remove(LuminaTaskData<T> item) => dataList.Remove(item);
-    public int Count => dataList.Count;
+    public void Add(LuminaTaskData<T> item) => DataList.Add(item);
+    public void Clear() => DataList.Clear();
+    public bool Contains(LuminaTaskData<T> item) => DataList.Contains(item);
+    public void CopyTo(LuminaTaskData<T>[] array, int arrayIndex) => DataList.CopyTo(array, arrayIndex);
+    public bool Remove(LuminaTaskData<T> item) => DataList.Remove(item);
+    public int Count => DataList.Count;
     public bool IsReadOnly => false;
     // End ICollection
     
@@ -59,7 +59,7 @@ public class LuminaTaskDataList<T> : IDrawable, ICollection<LuminaTaskData<T>> w
     
     private void DrawStandardDataList()
     {
-        foreach (var dataEntry in dataList)
+        foreach (var dataEntry in DataList)
         {
             ImGui.TableNextColumn();
             switch (this)
@@ -107,9 +107,9 @@ public class LuminaTaskDataList<T> : IDrawable, ICollection<LuminaTaskData<T>> w
         ImGui.TableNextColumn();
         ImGui.TextColored(KnownColor.Gray.AsVector4(), Strings.CurrentNumDrops);
                             
-        if (dataList.Count > 0)
+        if (DataList.Count > 0)
         {
-            foreach (var data in dataList)
+            foreach (var data in DataList)
             {
                 ImGui.TableNextColumn();
                 var luminaData = LuminaCache<ContentFinderCondition>.Instance.GetRow(data.RowId)!;
@@ -128,7 +128,7 @@ public class LuminaTaskDataList<T> : IDrawable, ICollection<LuminaTaskData<T>> w
 
     public void Update(ref bool dataChanged, Func<uint, bool> getTaskStatusFunction)
     {
-        foreach (var task in dataList)
+        foreach (var task in DataList)
         {
             var status = getTaskStatusFunction(task.RowId);
 
@@ -142,7 +142,7 @@ public class LuminaTaskDataList<T> : IDrawable, ICollection<LuminaTaskData<T>> w
 
     public void Reset()
     {
-        foreach (var task in dataList)
+        foreach (var task in DataList)
         {
             task.Complete = false;
             task.CurrentCount = 0;
