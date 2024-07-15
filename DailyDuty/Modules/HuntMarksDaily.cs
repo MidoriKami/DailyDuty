bb9cda064a9ff736e90b806d1f81e626a53942b8
MidoriKami <mi_kami@outlook.com>
2024-07-15 16:35:48 -0700

    Fix Hunt Marks Daily not showing

--- DailyDuty/Modules/HuntMarksDaily.cs
+++ DailyDuty/Modules/HuntMarksDaily.cs
@@ -5,11 +5,14 @@ using Lumina.Excel.GeneratedSheets;
 
 namespace DailyDuty.Modules;
 
-public abstract class HuntMarksDaily : HuntMarksBase {
+public class HuntMarksDaily : HuntMarksBase {
 	public override ModuleName ModuleName => ModuleName.HuntMarksDaily;
+
 	public override ModuleType ModuleType => ModuleType.Daily;
 
 	public override DateTime GetNextReset() => Time.NextDailyReset() + TimeSpan.FromMinutes(1);
+
+	public override TimeSpan GetModulePeriod() => TimeSpan.FromDays(1);
 
 	protected override void UpdateTaskLists() {
 		var luminaUpdater = new LuminaTaskUpdater<MobHuntOrderType>(this, order => order.Type is 1);
