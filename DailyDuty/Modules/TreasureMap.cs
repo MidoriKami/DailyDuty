2800d873858ba560db143903a1dfe270c129ef80
MidoriKami <mi_kami@outlook.com>
2024-09-01 17:18:31 -0700

    Completely Reset TreasureMap Module
    getting fed up with minor issues that make no sense.
    
    closes #217

--- DailyDuty/Modules/TreasureMap.cs
+++ DailyDuty/Modules/TreasureMap.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using DailyDuty.Classes;
@@ -6,7 +7,7 @@ using DailyDuty.Localization;
 using DailyDuty.Models;
 using DailyDuty.Modules.BaseModules;
 using Dalamud.Game.ClientState.Conditions;
-using Dalamud.Game.Inventory.InventoryEventArgTypes;
+using FFXIVClientStructs.FFXIV.Client.Game;
 using Lumina.Excel.GeneratedSheets;
 
 namespace DailyDuty.Modules;
@@ -25,34 +26,51 @@ public class TreasureMapData : ModuleData {
 	}
 }
 
-public class TreasureMap : Modules.Special<TreasureMapData, TreasureMapConfig> {
-	public override ModuleName ModuleName 
-		=> ModuleName.TreasureMap;
+public unsafe class TreasureMap : Modules.Special<TreasureMapData, TreasureMapConfig> {
+	public override ModuleName ModuleName => ModuleName.TreasureMap;
 
-	public override bool ShouldReset() 
-		=> DateTime.UtcNow > Data.NextReset;
+	public override DateTime GetNextReset() => DateTime.MaxValue;
 
-	public override DateTime GetNextReset() 
-		=> DateTime.MaxValue;
+	private List<TreasureHuntRank> treasureMaps = [];
+	private List<uint> inventoryMaps = [];
+	private bool gatheringStarted;
+
+	public override void Load() {
+		base.Load();
+
+		treasureMaps = Service.DataManager.GetExcelSheet<TreasureHuntRank>()!
+			.Where(map => map.ItemName.Row is not 0)
+			.ToList();
+	}
 
 	public override void Reset() {
 		Data.MapAvailable = true;
-		Data.NextReset = DateTime.MaxValue;
-		Config.Suppressed = false;
-		
+        
 		base.Reset();
 	}
 
-	public TreasureMap()
-		=> Service.GameInventory.ItemAddedExplicit += OnItemAdded;
+	public override void Update() {
+		if (Service.Condition[ConditionFlag.Gathering42] && !gatheringStarted) {
+			gatheringStarted = true;
+			OnGatheringStart();
+		} 
+		else if (!Service.Condition[ConditionFlag.Gathering42] && gatheringStarted) {
+			gatheringStarted = false;
+			OnGatheringStop();
+		}
+        
+		base.Update();
+	}
 
-	public override void Dispose()
-		=> Service.GameInventory.ItemAddedExplicit -= OnItemAdded;
+	private void OnGatheringStart() {
+		inventoryMaps.Clear();
+		inventoryMaps = GetInventoryTreasureMaps();
+	}
 
-	private void OnItemAdded(InventoryItemAddedArgs data) {
-		if (!Service.Condition.Any(ConditionFlag.Gathering, ConditionFlag.Gathering42)) return;
-		
-		if (Service.DataManager.GetExcelSheet<TreasureHuntRank>()!.Any(treasureHunt => treasureHunt.ItemName.Row == data.Item.ItemId)) {
+	private void OnGatheringStop() {
+		var newInventoryMaps = GetInventoryTreasureMaps();
+
+		if (newInventoryMaps.Count > inventoryMaps.Count) {
 			Data.MapAvailable = false;
 			Data.LastMapGatheredTime = DateTime.UtcNow;
 			Data.NextReset = Data.LastMapGatheredTime + TimeSpan.FromHours(18);
@@ -60,6 +78,15 @@ public class TreasureMap : Modules.Special<TreasureMapData, TreasureMapConfig> {
 			DataChanged = true;
 			ConfigChanged = true;
 		}
+	}
+
+	private List<uint> GetInventoryTreasureMaps() {
+		var mapsInInventory =
+			from map in treasureMaps
+			where InventoryManager.Instance()->GetInventoryItemCount(map.ItemName.Row) > 0
+			select map.ItemName.Row;
+        
+		return mapsInInventory.ToList();
 	}
 
 	protected override ModuleStatus GetModuleStatus() 
