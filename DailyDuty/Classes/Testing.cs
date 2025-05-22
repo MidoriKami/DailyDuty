4dd624f16e7e78f4e5a662165bdce01f23712d02
MidoriKami <mi_kami@outlook.com>
2025-05-21 23:09:47 -0700

    add timeline stub

--- DailyDuty/Classes/Testing.cs
+++ DailyDuty/Classes/Testing.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Numerics;
 using FFXIVClientStructs.FFXIV.Client.UI;
-using KamiToolKit.Classes;
-using KamiToolKit.Nodes.ComponentNodes.InputText;
 
 namespace DailyDuty.Classes;
 
 public unsafe class Testing : IDisposable{
-	private InputText? inputText;
 	
 	public Testing() {
 		System.NameplateAddonController.OnAttach += AttachNodes;
@@ -20,19 +16,10 @@ public unsafe class Testing : IDisposable{
 	}
 	
 	private void AttachNodes(AddonNamePlate* addon) {
-		inputText = new InputText {
-			Position = new Vector2(700.0f, 500.0f),
-			Size = new Vector2(152.0f, 28.0f),
-			IsVisible = true,
-		};
-		
-		System.NativeController.AttachToAddon(inputText, addon, addon->RootNode, NodePosition.AsLastChild);
+
 	}
 	
 	private void DetachNodes(AddonNamePlate* addon) {
-		System.NativeController.DetachFromAddon(inputText, addon, () => {
-			inputText?.Dispose();
-			inputText = null;
-		});
+
 	}
 }
